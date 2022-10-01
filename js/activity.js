f15b4d0478b2fcd704fc18a80abb84a9d6b1e4d8
Yinghai Yu <Y15653862659@outlook.com>
2022-10-01 14:31:35 -0700

    2

--- js/activity.js
+++ js/activity.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     $('.tdhighlight').click(function(event) {
+        // var bkcolor = $(this).css('background-color');
+        //     console.log('bkcolor: ' + bkcolor);
+        //     if (bkcolor === 'rgb(0, 255, 0)') {
+        //         $(this).css({'background-color': '#eeffe6'});
+        //     } else {
+        //         $(this).css({'background-color': '#00ff00'});
+        //     }
+        //     event.stopPropagation();
+
         var content = $(this).text();
         if (content != 'Not Available') {
             $(this).toggleClass('tdhighlight');
@@ -7,21 +16,16 @@ $(document).ready(function() {
                 $('#displaySelected').css('visibility', 'visible');
                 $('#displaySelected').css('margin-top', '2em');
                 $('#result').append('<p>' + content + '</p>');
+                $(this).css({'background-color': '#00ff00'});
             } else {
+                $(this).css({'background-color': '#eeffe6'});
                 $('#result p:contains(' + content + ')').remove();
                 if ($('#result').has('p').length == false) {
                     $('#displaySelected').css('visibility', 'hidden');
                     $('#displaySelected').css('margin-top', '0');
                 }
             }
-            var bkcolor = $(this).css('background-color');
-            console.log('bkcolor: ' + bkcolor);
-            if (bkcolor === 'rgb(0, 255, 0)') {
-                $(this).css({'background-color': '#eeffe6'});
-            } else {
-                $(this).css({'background-color': '#00ff00'});
-            }
-            event.stopPropagation();
+            
             
         }
        
